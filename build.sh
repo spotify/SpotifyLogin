706e3f41c9bfe60e2549bb53a96f92a4cfcff802
Roy Marmelstein <marmelroy@gmail.com>
2017-08-29 10:18:29 +0200

    Travis: add CI support and build script

---
+++ build.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# **** Update me when new Xcode versions are released! ****
+PLATFORM="platform=iOS Simulator,OS=11.0,name=iPhone 7"
+SDK="iphonesimulator"
+
+
+# It is pitch black.
+set -e
+function trap_handler() {
+    echo -e "\n\nOh no! You walked directly into the slavering fangs of a lurking grue!"
+    echo "**** You have died ****"
+    exit 255
+}
+trap trap_handler INT TERM EXIT
+
+
+MODE="$1"
+
+if [ "$MODE" = "framework" ]; then
+    echo "Building and testing SpotifyLogin.framework."
+    xcodebuild \
+        -project SpotifyLogin.xcodeproj \
+        -scheme SpotifyLogin \
+        -sdk "$SDK" \
+        -destination "$PLATFORM" \
+        build test
+    trap - EXIT
+    exit 0
+fi
+
+if [ "$MODE" = "examples" ]; then
+    echo "Building and testing all SpotifyLogin examples."
+
+    for example in examples/*/; do
+        echo "Building $example."
+        pod install --project-directory=$example
+        xcodebuild \
+            -workspace "${example}Sample.xcworkspace" \
+            -scheme Sample \
+            -sdk "$SDK" \
+            -destination "$PLATFORM" \
+            build
+    done
+    trap - EXIT
+    exit 0
+fi
+
+echo "Unrecognised mode '$MODE'."
